3f339304683ba33099b403159888fa5788b6ba78
nytsdayo <nyantsutsu@gmail.com>
2024-02-09 15:07:55 +0900

    React Setting

---
+++ my-react-app/src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCWFW4n3Di65PNOi1az-S0wsQ2FRIbx68",
+  authDomain: "enjoy-todolist.firebaseapp.com",
+  projectId: "enjoy-todolist",
+  storageBucket: "enjoy-todolist.appspot.com",
+  messagingSenderId: "420417356672",
+  appId: "1:420417356672:web:e6238a43363e528b017765"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
