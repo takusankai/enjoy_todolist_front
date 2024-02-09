4c39156af8f04e88d9e34f11267aa085e417bda3
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-02-10 02:42:32 +0900

    みんなのToDoリストを編集。formanticUIをインストール。

---
+++ my-react-app/src/components/OtherUsersTodo/MessageContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const MessageContainer = ({username, comment}) => (
+  <div class="ui big message">
+        { comment }
+  </div>
+);
--- my-react-app/src/pages/OtherUsersToDo.jsx
+++ my-react-app/src/pages/OtherUsersToDo.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import "./OtherUsersToDo.css"
+import React from 'react';
+import "./OtherUsersToDo.css";
+import { MessageContainer } from "../components/OtherUsersTodo/MessageContainer";
 
 function OtherUsersToDo() {
+  const userDetails = [
+    {
+      "username": "JohnDoe",
+      "comment": "This is a great post!",
+      "post_date": "2024-02-10T12:30:00"
+    },
+    {
+      "username": "JaneSmith",
+      "comment": "Thanks for sharing!",
+      "post_date": "2024-02-10T13:45:00"
+    },
+    {
+      "username": "BobJohnson",
+      "comment": "Interesting discussion!",
+      "post_date": "2024-02-10T14:20:00"
+    }
+  ];
+
   return (
     <div className="OtherUsersToDo">
-      ここはOtherUsersToDoページです。
+      {userDetails.map((user, index) => (
+        <MessageContainer
+          key={index}
+          username={user.username}
+          comment={user.comment}
+          postDate={user.post_date}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default OtherUsersToDo
+export default OtherUsersToDo;
