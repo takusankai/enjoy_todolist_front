4ae79589a71dfd4a6cf3683da727e672801f4fe3
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-02-10 03:49:18 +0900

    みんなのToDo一覧のレイアウトを変更。

--- my-react-app/src/components/OtherUsersTodo/MessageContainer.jsx
+++ my-react-app/src/components/OtherUsersTodo/MessageContainer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-export const MessageContainer = ({username, comment}) => (
-  <div class="ui big message">
+export const MessageContainer = ({userName, comment, postDate}) => (
+  <div>
+    <div className="ui big message">
         { comment }
+    </div>
+    <div className="ui compact menu">
+      <div className="item">
+        <i class="pink like icon"></i>Like
+        <div className="left floating ui pink label">22</div>
+      </div>
+      <div className="item">
+        <i className="icon blue users"></i>
+        { userName }
+      </div>
+      <div className="item">
+        <i className="orange clock icon"></i>
+        { postDate }
+      </div>
+    </div>
+    <div className="ui hidden divider"></div>
+    <div className="ui hidden divider"></div>
   </div>
 );
