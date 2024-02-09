85d0c9b442b745f6cca7ac327a4635e25bdbea5b
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-02-10 04:16:40 +0900

    みんなの達成リストのアイコンを変更。

--- my-react-app/src/components/OtherUsersTodo/Header.jsx
+++ my-react-app/src/components/OtherUsersTodo/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => (
     <div className="ui container">
       <h1 class="ui center aligned header">
         <i class="large icons">
-          <i class="puzzle icon"></i>
+          <i class="brown shoe prints icon"></i>
           <i class="inverted corner add icon"></i>
         </i>
         みんなの ToDo 一覧
