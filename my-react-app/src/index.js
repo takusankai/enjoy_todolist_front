993d1363b9445ef0d7ec00cf354575895a9bb05e
nytsdayo <nyantsutsu@gmail.com>
2024-02-10 14:39:46 +0900

    FireBaseのAuthContextを設定し、onAuthStateChangedを使えるようにしました。

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider> {/* AuthProviderをRouterの外側に配置 */}
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
