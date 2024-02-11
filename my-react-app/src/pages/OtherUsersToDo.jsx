28e903692435da25b39ccc9d5d816ebea03fd12f
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-02-11 12:39:34 +0900

    変数名を変更。

--- my-react-app/src/pages/OtherUsersToDo.jsx
+++ my-react-app/src/pages/OtherUsersToDo.jsx
@@ -9,54 +9,54 @@ function OtherUsersToDo()
 
   const userDetails = [
     {
-      "username": "JohnDoe",
-      "comment": "This is a great post!",
-      "post_date": "2024-02-10T12:30:00"
+      "userName": "JohnDoe",
+      "todoContent": "This is a great post!",
+      "createdAt": "2024-02-10T12:30:00"
     },
     {
-      "username": "JaneSmith",
-      "comment": "Thanks for sharing!",
-      "post_date": "2024-02-10T13:45:00"
+      "userName": "JaneSmith",
+      "todoContent": "Thanks for sharing!",
+      "createdAt": "2024-02-10T13:45:00"
     },
     {
-      "username": "BobJohnson",
-      "comment": "Interesting discussion!",
-      "post_date": "2024-02-10T14:20:00"
+      "userName": "BobJohnson",
+      "todoContent": "Interesting discussion!",
+      "createdAt": "2024-02-10T14:20:00"
     },
     {
-      "username": "AliceWilliams",
-      "comment": "I agree with the points raised!",
-      "post_date": "2024-02-10T15:10:00"
+      "userName": "AliceWilliams",
+      "todoContent": "I agree with the points raised!",
+      "createdAt": "2024-02-10T15:10:00"
     },
     {
-      "username": "CharlieBrown",
-      "comment": "Looking forward to more posts!",
-      "post_date": "2024-02-10T16:05:00"
+      "userName": "CharlieBrown",
+      "todoContent": "Looking forward to more posts!",
+      "createdAt": "2024-02-10T16:05:00"
     },
     {
-      "username": "EvaMiller",
-      "comment": "Great insights! Keep it up!",
-      "post_date": "2024-02-10T17:30:00"
+      "userName": "EvaMiller",
+      "todoContent": "Great insights! Keep it up!",
+      "createdAt": "2024-02-10T17:30:00"
     },
     {
-      "username": "DavidLee",
-      "comment": "Wonderful content!",
-      "post_date": "2024-02-10T18:15:00"
+      "userName": "DavidLee",
+      "todoContent": "Wonderful content!",
+      "createdAt": "2024-02-10T18:15:00"
     },
     {
-      "username": "GraceTaylor",
-      "comment": "I learned a lot from this post!",
-      "post_date": "2024-02-10T19:20:00"
+      "userName": "GraceTaylor",
+      "todoContent": "I learned a lot from this post!",
+      "createdAt": "2024-02-10T19:20:00"
     },
     {
-      "username": "SamuelClark",
-      "comment": "Interesting perspectives!",
-      "post_date": "2024-02-10T20:00:00"
+      "userName": "SamuelClark",
+      "todoContent": "Interesting perspectives!",
+      "createdAt": "2024-02-10T20:00:00"
     },
     {
-      "username": "OliviaSmith",
-      "comment": "Thanks for the informative post!",
-      "post_date": "2024-02-10T21:10:00"
+      "userName": "OliviaSmith",
+      "todoContent": "Thanks for the informative post!",
+      "createdAt": "2024-02-10T21:10:00"
     }
   ];
 
@@ -89,9 +89,9 @@ function getUserTodos()
     .then((datas) => {
       const jsonData = datas.map(data => ({
         // 「フロントでの変数名 : バックでの変数名」に map 変換
-        userName   : data.username,
-        todoName   : data.TodoName,
-        createTime : data.CreateTime,
+        userName    : data.username,
+        todoContent : data.TodoName,
+        createdAt   : data.CreateTime,
       }));
       return jsonData;
     })
